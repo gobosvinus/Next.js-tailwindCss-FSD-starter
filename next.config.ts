7bbcd700ee7601a7d8f41a34a7bc07bb368e7b5e
Владислав Беденко <vladsbook@Vladislavs-MacBook-Pro.local>
2026-02-09 22:07:51 +0800

    add svgr support

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Конфиг для SVGR который сохраняет viewBox of the svg
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: { icon: true },
+          },
+        ],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
